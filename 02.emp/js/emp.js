d5d51657799ccfb2a5947945d732432caca770b0
ming.jia-mac <ming.jia@dd-car.com.cn>
2017-12-10 22:37:34 +0800

    Initial commit

---
+++ 02.emp/js/emp.js
@@ -0,0 +1,45 @@
+var vm = new Vue({
+  el : "#app" ,
+  data : {
+    message : 'Hi',
+    title : 'Vue Demo'
+
+  } ,
+  filters : {
+
+  } ,
+  mounted : function(){
+    this.$nextTick(function(){
+      this.cartView()
+    })
+  } ,
+  methods : {
+    add : function(){
+      console.log(this.$el.innerText)
+    } ,
+    cartView : function() {
+       this.message = 'Hello Vue'
+    }
+
+  }
+})
+
+
+////////////
+
+var cart = new Vue({
+  el : '#cart' ,
+  data : {
+
+  },
+  mounted : function(){
+    this.getCart();
+  },
+  methods : {
+    getCart : function() {
+      this.$http.get("data/emp.json").then(function(res){
+        console.log(res.data)
+      });
+    }
+  }
+})
