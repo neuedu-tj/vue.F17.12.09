2229b9cfb9f358da670ad2e166e80d6d35cf5ab6
neuedu-tj <13752284294@qq.com>
2018-03-22 08:31:07 +0800

    Address 模块  loadMore

--- 03.auto/js/addr.js
+++ 03.auto/js/addr.js
@@ -1,20 +1,39 @@
+
 var vm = new Vue({
 
     el : "#addr",
     data : {
+        slied_status : false,
+        limit : 3 , 
         addrs : []
-
     },
     mounted : function () {
         this.$nextTick(function () {
-            vm.init();
+            this.init();
         });
     },
     methods : {
         init : function () {
-            this.$http.get("data/address.json").then(res =>{
-                vm.addrs = res.body.result;
+            this.$http.get("data/address.json").then(res => {
+
+                this.addrs = res.body.result;
             })
+        },
+        loadMore : function () {
+            this.slied_status = !this.slied_status;
+
+            if (this.slied_status) {
+                this.limit = this.addrs.length;
+            } else {
+                this.limit = 3;
+            }
+            
+        }
+        
+    } ,
+    computed : {
+        filterAddr : function () {
+            return this.addrs.slice(0,this.limit);
         }
     }
 
